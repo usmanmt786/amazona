4a66ca9b0febb9834287e5d27b2fc8ae9fe396a0
Usman Faris M T <baumonmt786@gmail.com>
2023-01-30 17:58:57 +0530

    register screen finished

--- pages/register.js
+++ pages/register.js
@@ -7,9 +7,9 @@ import {toast} from 'react-toastify'
 import {useEffect} from 'react'
 import {useSession} from 'next-auth/react'
 import {useRouter} from 'next/router'
+import axios from 'axios'
 
-
-function LoginScreen(){
+function RegisterScreen(){
 const router = useRouter()
 const { data:session} = useSession()
 const {redirect}=router.query
@@ -21,10 +21,16 @@ router.push(redirect || '/')
 const {
 handleSubmit,
 register,
+getValues,
 formState: {errors},
 } = useForm();
-const  submitHandler=async ({email,password})=>{
+const  submitHandler=async ({name,email,password})=>{
 try{
+await axios.post('/api/auth/signup',{
+name,
+email,
+password
+})
 const result = await signIn('credentials',{ 
 
 redirect:false,
@@ -40,16 +46,27 @@ toast.error(getError(err))
 }
 }
 return(
-<Layout title="Login">
+<Layout title="Create Account">
 <form className="mx-auto max-w-screen-md" onSubmit={handleSubmit(submitHandler)}>
-<h1 className="text-xl mb-4">Login</h1>
+<h1 className="text-xl mb-4">Create Account</h1>
+<div className="mb-4">
+<label htmlFor="password">Name</label>
+<input 
+type="text" 
+id="name" 
+className="w-full"
+{...register('name',{required:"Please enter name",
+})}
+autoFocus/>
+{errors.name && <div className="text-red-500">{errors.name.message}</div>  }
+</div>
 <div className="mb-4">
 <label htmlFor="email">Email</label>
 <input 
 type="email" 
 id="email" 
 className="w-full" 
-autoFocus
+
 {...register('email',{required:"Please enter email",
 pattern:{
 value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
@@ -72,15 +89,28 @@ minLength:{value:6, message: 'Password should be more than 5 characters'}
 {errors.password && <div className="text-red-500">{errors.password.message}</div>  }
 </div>
 <div className="mb-4">
-<button className="primary-button">Login</button>
+<label htmlFor="confirmPassword">Confirm Password</label>
+<input 
+type="password" 
+id="caonfirmPassword" 
+className="w-full"
+{...register('confirmPassword',{required:"Please enter confirm password",
+validate: (value) => value===getValues('password'),
+minLength:{value:6, message: "Confirm password should be more than 5 characters"}
+})}/>
+{errors.confirmPassword && <div className="text-red-500">{errors.confirmPassword.message}</div>}
+{errors.confirmPassword && errors.confirmPassword.type === 'validate'&& <div className="text-red-500">Password does not match</div>}
+</div>
+<div className="mb-4">
+<button className="primary-button">Register</button>
 </div>
 <div className="mb-4">
 Don't have an account? &nbsp;
-<Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
+<Link href={`/register?redirect=${redirect || '/'}`} >Register</Link>
 </div>
 </form>
 </Layout>
 )
 }
 
-export default LoginScreen
+export default RegisterScreen
